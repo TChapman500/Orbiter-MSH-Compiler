afbc564380976302f9620c45bf8459bd82874830
Timothy Chapman <tchapman500@zohomail.com>
2022-06-02 20:58:11 -0400

    Update Features

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include "Mesh.h"
 
+struct vtx9 { float x, y, z, nx, ny, nz, tu, tv; };
 struct vtx3 { float x, y, z; };
 struct vtx2 { float x, y; };
 
@@ -278,6 +279,16 @@ public:
 	}
 };
 
+struct cmsh_header
+{
+	char Header[8];
+	int GroupCount;
+	int MaterialCount;
+	int TextureCount;
+	int VertexComponents : 1;
+	int MaterialNames : 1;
+};
+
 
 int main(int argCount, char **argList)
 {
@@ -289,43 +300,29 @@ int main(int argCount, char **argList)
 	bool outputNext = false;
 	bool noMatNames = false;
 
-	if (argCount == 1)
+	for (int i = 1; i < argCount; i++)
 	{
-		char buffer[256];
-		ZeroMemory(buffer, 256);
-		std::cout << "Input File> ";
-		std::cin >> buffer;
-		int length = strlen(buffer) + 1;
-		inputFile = new char[length];
-		for (int i = 0; i < length; i++) inputFile[i] = buffer[i];
-	}
-	else
-	{
-		for (int i = 1; i < argCount; i++)
+		if (!inputNext && !outputNext)
 		{
-			if (!inputNext && !outputNext)
+			if (strcmp(argList[i], "-s") == 0) straightConvert = true;
+			else if (strcmp(argList[i], "-i") == 0) inputNext = true;
+			else if (strcmp(argList[i], "-o") == 0) outputNext = true;
+			else if (strcmp(argList[i], "-m") == 0) noMatNames = true;
+			else if (!inputFile) inputFile = argList[i];
+			else if (!outputFile) outputFile = argList[i];
+		}
+		else
+		{
+			if (inputNext)
 			{
-				if (strcmp(argList[i], "-s") == 0) straightConvert = true;
-				else if (strcmp(argList[i], "-i") == 0) inputNext = true;
-				else if (strcmp(argList[i], "-o") == 0) outputNext = true;
-				else if (strcmp(argList[i], "-m") == 0) noMatNames = true;
-				else if (!inputFile) inputFile = argList[i];
-				else if (!outputFile) outputFile = argList[i];
+				inputNext = false;
+				inputFile = argList[i];
 			}
-			else
+			else if (outputNext)
 			{
-				if (inputNext)
-				{
-					inputNext = false;
-					inputFile = argList[i];
-				}
-				else if (outputNext)
-				{
-					outputNext = false;
-					outputFile = argList[i];
-				}
+				outputNext = false;
+				outputFile = argList[i];
 			}
-
 		}
 	}
 
@@ -364,7 +361,8 @@ int main(int argCount, char **argList)
 		if (index == -1)
 		{
 			// Set output file and designate extension.
-			outputFile = new(std::nothrow) char[length + 6];
+			int outLength = length + 6;
+			outputFile = new(std::nothrow) char[outLength];
 			if (!outputFile)
 			{
 				std::cout << "Error:  Could not generate output file." << std::endl;
@@ -388,7 +386,8 @@ int main(int argCount, char **argList)
 			int outLength = length - extLength + 4;	// Output length.
 			int lenNoExt = length - extLength + 1;	// Make sure to copy the decimal point.
 
-			outputFile = new(std::nothrow) char[outLength + 1];
+			int finalLength = outLength + 1;
+			outputFile = new(std::nothrow) char[finalLength];
 			if (!outputFile)
 			{
 				std::cout << "Error:  Could not generate output file." << std::endl;
@@ -455,17 +454,8 @@ int main(int argCount, char **argList)
 		return -4;
 	}
 
-	struct cmesh_header
-	{
-		char Header[8];
-		int GroupCount;
-		int MaterialCount;
-		int TextureCount;
-		int VertexComponents : 1;
-		int MaterialNames : 1;
-	};
-	cmesh_header header;
-	ZeroMemory(&header, sizeof(cmesh_header));
+	cmsh_header header;
+	ZeroMemory(&header, sizeof(cmsh_header));
 	header.Header[0] = '_';
 	header.Header[1] = 'C';
 	header.Header[2] = 'M';
@@ -480,9 +470,9 @@ int main(int argCount, char **argList)
 	header.VertexComponents = straightConvert ? 0 : 1;
 	header.MaterialNames = noMatNames ? 0 : 1;
 
-	std::cout << "#Groups:\t" << header.GroupCount << std::endl;
-	std::cout << "#Materials:\t" << header.MaterialCount << std::endl;
-	std::cout << "#Textures:\t" << header.TextureCount << std::endl << std::endl;
+	std::cout << "Group Count:\t" << header.GroupCount << std::endl;
+	std::cout << "Material Count:\t" << header.MaterialCount << std::endl;
+	std::cout << "Texture Count:\t" << header.TextureCount << std::endl << std::endl;
 
 
 	std::ofstream oMeshFile(outputFile, std::ios::binary);
@@ -494,7 +484,7 @@ int main(int argCount, char **argList)
 		return -5;
 	}
 
-	oMeshFile.write((char *)&header, sizeof(cmesh_header));
+	oMeshFile.write((char *)&header, sizeof(cmsh_header));
 
 	// Write mesh group data.
 	if (oMesh->GroupList)
@@ -505,10 +495,10 @@ int main(int argCount, char **argList)
 			ExMeshGroup *current = oMesh->GroupList[i];
 
 			std::cout << "Mesh Group: " << current->Label << std::endl;
-			std::cout << "\tMat:\t\t" << current->MaterialIndex << std::endl;
-			std::cout << "\tTex:\t\t" << current->TextureIndex << std::endl;
-			std::cout << "\t#Vertices:\t" << current->VertexCount << std::endl;
-			std::cout << "\t#Indices:\t" << current->IndexCount << std::endl << std::endl;
+			std::cout << "\tMat Index:\t" << current->MaterialIndex << std::endl;
+			std::cout << "\tTexture Index:\t" << current->TextureIndex << std::endl;
+			std::cout << "\tVertex Count:\t" << current->VertexCount << std::endl;
+			std::cout << "\tIndex Count:\t" << current->IndexCount << std::endl << std::endl;
 
 			// Write group header.
 			int length = strlen(current->Label) + 1;
